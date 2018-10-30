0281b432bed9317ef35615f913ee572b5bbfd0b8
Elona <eloniuke@gmail.com>
2018-10-30 14:20:16 +0100

    Update new.py

--- new.py
+++ new.py
@@ -60,11 +60,11 @@ if z1serial.is_open:
                 switchCase[messageState]()
                 if newMeasReady:
                     newMeasReady = False
-                    inputBuffer = [None]*receiveCounter
+                    inputBuffer = [0]*receiveCounter
                     sum1 = 0
                     sum2 = 0
                     for i in range(1, len(inputBuffer)-2):
-                        sum1 = bin(sum1+inputBuffer[i]) & bin(0xff) % 255
+                        sum1 = int(sum1+inputBuffer[i]) & int(0xff) % int(255)
                         #sum2 = (sum2 + sum1) % 255
                 #checksumcalculated = ((sum2 & 0xff) << 8) | (sum1 & 0xff)                
         else:
