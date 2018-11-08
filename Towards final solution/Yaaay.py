a975430aa0d0f51dbe69ee377061c9b2cc9a9637
Elona <eloniuke@gmail.com>
2018-11-08 14:08:38 +0100

    Update Towards final solution/Yaaay.py

--- Towards final solution/Yaaay.py
+++ Towards final solution/Yaaay.py
@@ -117,7 +117,7 @@ if z1serial.is_open:
                 measurementTimer = int(round(time.time()*1000))
                 measurementsUse = measurementToUseChooser(measurementHistory, numSats, int(round(time.time()*1000)))
                 for i in range(len(measurementsUse.measurementsUse)):
-                    print('1 ', i, ' ', measurementsUse.measurementsUse[i].transmitterID)
+                    print(len(measurementsUse.measurementsUse), '1 ', i, ' ', measurementsUse.measurementsUse[i].transmitterID)
                 if measurementsUse.usePosCalc:
                    measuremetForward = ForwardCalculation(measurementHistory, measurementsUse, int(round(time.time()*1000)))
                    print('2 ', measuremetForward.measurementsUse[0].transmitterID)
