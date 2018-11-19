77bb57349c73170e4a18b6500dc20751dee090b9
Elona <eloniuke@gmail.com>
2018-11-19 11:16:30 +0100

    first part

--- Towards final solution/robotClass.py
+++ Towards final solution/robotClass.py
@@ -3,6 +3,8 @@
 import math
 import numpy as np
 
+#For Points we use np.array
+
 class WayPoints:
 
 	def __init__(self):
@@ -10,7 +12,22 @@ class WayPoints:
 
 	#add a waypoint
 	def addWayPoint(self, x, y):
-		tempPoint = (x, y)
+		tempPoint = np.array([x, y])
 		self.wayPoint.append(tempPoint)
 
 	#remove a waypoint
+
+class PlanThePath:
+#Add functions to find the next point, calculate needed values
+   
+	def __init__(self, W1, W2):
+		self.W1 = W1
+		self.W2 = W2
+		self.u0 = np.array([0, 0])
+
+	def plan(self):
+		self.u0 = self.W2 - self.W1
+		norm = np.linalg.norm(self.u0)
+		self.u0 = np.divide(self.u0, norm)
+
+		
--- Towards final solution/robotMain.py
+++ Towards final solution/robotMain.py
@@ -10,3 +10,8 @@ map1 = WayPoints() #the object that stores all way points of a map
 map1.addWayPoint(1, 0)
 map1.addWayPoint(0, 1)
 
+#print(map1.wayPoint[0])
+
+path = PlanThePath(map1.wayPoint[0], map1.wayPoint[1])
+#print(path.W1, path.W2)
+path.plan()
