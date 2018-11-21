47e29bb96e14b61677197c10c43fd6afe8da899b
Elona <eloniuke@gmail.com>
2018-11-21 08:43:05 +0100

    -

--- Towards final solution/robotClass.py
+++ Towards final solution/robotClass.py
@@ -13,6 +13,7 @@ class WayPoints:
 	#add a waypoint
 	def addWayPoint(self, x, y):
 		tempPoint = np.array([x, y])
+		#tempPoint = np.transpose(tempPoint)
 		self.wayPoint.append(tempPoint)
 
 	#remove a waypoint
@@ -31,12 +32,35 @@ class PlanThePath:
 		norm = np.linalg.norm(self.u0)
 		self.u0 = np.divide(self.u0, norm)
 		x = np.array([2, 2])
+		#x = np.transpose(x)
 		u = np.array([1, 0])
+		#u = np.transpose(u)
 		omf = 0.1
 		dt = 0.1
-		R = np.array([[0, -1], [1, 0]])
+		Robot = np.array([[0, -1], [1, 0]])
 
-		while not atThePoint:
+		tempCounter = 0
+		while tempCounter <= 10:
 			#If not near the final position
-			print(atThePoint)
-			atThePoint = True
+			tempCounter += 1
+			#print(atThePoint)
+			#print('x + dt', x+dt)
+			#print('dt * u', dt * u)
+			#print('dt * u * omf', dt * u * omf)
+			x = x + dt * u * omf
+			#print('x + revious', x)
+			xf = self.W2 + (omf - np.transpose(self.u0) * (self.W2 - x)) * self.u0
+			#print(xf)
+			v = xf - x
+			v = v / np.linalg.norm(v)
+			omd = (u[0] * v[1] - u[1] * v[0]) * 2
+			R = Robot * dt
+			ru = np.array([R[0][0] * u[0] + R[0][1] * u[1], R[1][0] * u[0] + R[1][1] * u[1]])
+			ru = ru * omd
+			#print('ru', ru, 'u', u)
+			print(u + ru)
+			#print(ru)
+			#u = u + dt * ru * u * omd
+			#print(R * u)
+			#print(R[0][0], R[0][1], R[1][0], R[1][1])
+			#print('u =', u)
