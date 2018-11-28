97a9c41af86e9e82a2d2fc4d10f59ef5403f45b5
mrosssi16 <mrossi16@student.aau.dk>
2018-11-28 12:41:36 +0000

    Create notmain.py

---
+++ Sandbox/notmain.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import rospy
+class numbers:
+    def __init__(self):
+        self.one = one
+        
+    def numberss(self):
+        check = True
+        while check:
+            for n in range(0, 15):
+                self.one = n
+                print(self.one)
+                if n == 14:
+                    check = False
