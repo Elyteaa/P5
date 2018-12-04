45fd7293066ab380e41af07d09d89e7e562edf8a
Elona <eloniuke@gmail.com>
2018-12-04 10:27:42 +0100

    Update robotMain.py

--- Sandbox/ROS/robotMain.py
+++ Sandbox/ROS/robotMain.py
@@ -7,11 +7,11 @@ from robotClass import *
 from std_msgs.msg import Float32MultiArray
 
 #positions = []
-class Subscriber(object):
+class Listener():
 
     def __init__(self):
         self.position = []
-        rospy.Subscriber("position", Float32MultiArray, callback)
+        self.sub = rospy.Subscriber("position", Float32MultiArray, self.callback)
 
     def callback(data):
         rospy.loginfo("I receive %s", data.data)
@@ -20,42 +20,42 @@ class Subscriber(object):
     #def start(self):
     #   self
 
-def callback(data):
-    rospy.loginfo("I receive %s", data.data)
-    positions = data.data
+#def callback(data):
+ #   rospy.loginfo("I receive %s", data.data)
+ #   positions = data.data
     #print(positions)
     
-def listener():
+#def listener():
 
     # In ROS, nodes are uniquely named. If two nodes with the same
     # node are launched, the previous one is kicked off. The
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+    #rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber("position", Float32MultiArray, callback)
+    #rospy.Subscriber("position", Float32MultiArray, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+    #rospy.spin()
     
 if __name__ == '__main__':
-    while True:
-        print('yes')
-        rospy.init_node('listener', anonymous=True)
-        my_node = Subscriber()
-        #listener()
-        rospy.Subscriber("position", Float32MultiArray, callback)
-        rospy.spin()
-        print("position", positions)
-        map1 = WayPoints() #the object that stores all way points of a map
+    #while True:
+    print('yes')
+    rospy.init_node('listener', anonymous=True)
+    my_node = Listener()
+    #listener()
+    #rospy.Subscriber("position", Float32MultiArray, callback)
+    rospy.spin()
+    print("position", positions)
+    map1 = WayPoints() #the object that stores all way points of a map
 
-        map1.addWayPoint(1, 0)
-        map1.addWayPoint(0, 1)
+    map1.addWayPoint(1, 0)
+    map1.addWayPoint(0, 1)
 
-        #print(map1.wayPoint[0])
+    #print(map1.wayPoint[0])
 
-        path = PlanThePath(map1.wayPoint[0], map1.wayPoint[1])
-        #print(path.W1, path.W2)
-        path.plan()
-        rospy.spin()
+    path = PlanThePath(map1.wayPoint[0], map1.wayPoint[1])
+    #print(path.W1, path.W2)
+    path.plan()
+    rospy.spin()
